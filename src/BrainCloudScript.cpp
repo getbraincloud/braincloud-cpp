0b9b16c44f75cb8189f57f6456b26858cb301926
ruth0048 <31827673+ruth0048@users.noreply.github.com>
2020-03-03 12:08:17 -0500

    Updates to UTC time calls no tests yet

--- src/BrainCloudScript.cpp
+++ src/BrainCloudScript.cpp
@@ -50,6 +50,24 @@ namespace BrainCloud
 		m_client->sendRequest(sc);
 	}
 
+	void BrainCloudScript::scheduleRunScriptUTCv2(const char * in_scriptName, const char * in_jsonScriptData, const tm* in_startDateInUTC, IServerCallback * in_callback)
+	{
+		Json::Value message;
+		message[OperationParam::ScriptServiceRunScriptName.getValue()] = in_scriptName;
+
+		if (StringUtil::IsOptionalParameterValid(in_jsonScriptData)) {
+			Json::Value jsonData = JsonUtil::jsonStringToValue(in_jsonScriptData);
+			message[OperationParam::ScriptServiceRunScriptData.getValue()] = jsonData;
+		}
+
+		//struct tm timeInfo = *in_startDateInUTC;
+		//message[OperationParam::ScriptServiceStartDateUTC.getValue()] = (Json::Int64) ((int64_t)internal_timegm(&timeInfo)) * 1000;
+		message[OperationParam::ScriptServiceStartDateUTC.getValue()] = (Json::Int64) in_startDateInUTC;
+
+		ServerCall * sc = new ServerCall(ServiceName::Script, ServiceOperation::ScheduleCloudScript, message, in_callback);
+		m_client->sendRequest(sc);
+	}
+
 	void BrainCloudScript::scheduleRunScriptMinutes(const char * in_scriptName, const char * in_jsonScriptData, int in_minutesFromNow, IServerCallback * in_callback)
 	{
 		Json::Value message;
