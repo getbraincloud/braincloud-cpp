0c2a094ef6178435edd6f11e1fef0c0973a0309e
Daivuk <daivuk@gmail.com>
2021-12-07 07:41:15 -0500

    BCLOUD-1314 fixed wrong doc for getSocialLB

--- include/braincloud/BrainCloudSocialLeaderboard.h
+++ include/braincloud/BrainCloudSocialLeaderboard.h
@@ -43,10 +43,11 @@ namespace BrainCloud
 		/**
 		 * Method returns the social leaderboard. A player's social leaderboard is
 		 * comprised of players who are recognized as being your friend.
-		 * For now, this applies solely to Facebook connected players who are
-		 * friends with the logged in player (who also must be Facebook connected).
-		 * In the future this will expand to other identification means (such as
-		 * Game Center, Google circles etc).
+         * 
+         * The getSocialLeaderboard will retrieve all friends from all friend platforms, so
+         * - all external friends (Facebook, Steam, PlaystationNetwork)
+         * - all internal friends (brainCloud)
+         * - plus "self".
 		 *
 		 * Leaderboards entries contain the player's score and optionally, some user-defined
 		 * data associated with the score. The currently logged in player will also
@@ -66,10 +67,11 @@ namespace BrainCloud
 		/**
 		 * Method returns the social leaderboard by its version. A player's social leaderboard is
 		 * comprised of players who are recognized as being your friend.
-		 * For now, this applies solely to Facebook connected players who are
-		 * friends with the logged in player (who also must be Facebook connected).
-		 * In the future this will expand to other identification means (such as
-		 * Game Center, Google circles etc).
+         * 
+         * The getSocialLeaderboard will retrieve all friends from all friend platforms, so
+         * - all external friends (Facebook, Steam, PlaystationNetwork)
+         * - all internal friends (brainCloud)
+         * - plus "self".
 		 *
 		 * Leaderboards entries contain the player's score and optionally, some user-defined
 		 * data associated with the score. The currently logged in player will also
