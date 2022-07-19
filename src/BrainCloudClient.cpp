e2d1846b572ab698a1553c166fbf5d8679ad7ed7
franco <82105876+francobithead@users.noreply.github.com>
2022-07-19 11:39:19 -0400

    Updated the version to 4.12.0

--- src/BrainCloudClient.cpp
+++ src/BrainCloudClient.cpp
@@ -27,7 +27,7 @@ namespace BrainCloud
             "Singleton usage is disabled. If called by mistake, use your own variable that holds an instance of the bcWrapper/bcClient.";
 
     BrainCloudClient * BrainCloudClient::_instance = NULL;
-    std::string BrainCloudClient::s_brainCloudClientVersion = "4.11.0";
+    std::string BrainCloudClient::s_brainCloudClientVersion = "4.12.0";
     const char* BC_SERVER_URL = "https://api.braincloudservers.com/dispatcherv2"; 
 
     /**
