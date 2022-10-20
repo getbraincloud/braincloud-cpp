edb545f3323a4da6c598c48c3be40c4dff36eaf1
Joanne Hoar <joanneh@bitheads.com>
2022-10-20 13:35:50 -0600

    BCLOUD-3011 Tests for blockchain

---
+++ tests/src/TestBCBlockchain.cpp
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "gtest/gtest.h"
+#include "braincloud/BrainCloudClient.h"
+#include "braincloud/BrainCloudBlockchain.h"
+#include "TestResult.h"
+#include "json/json.h"
+#include "TestBCBlockchain.h"
+#include "braincloud/reason_codes.h"
+#include "braincloud/http_codes.h"
+
+
+using namespace BrainCloud;
+
+
+TEST_F(TestBCBlockchain, GetBlockchainItems)
+{
+	TestResult tr;
+
+	Json::FastWriter fw;
+	m_bc->getBlockchainService()->GetBlockchainItems(_defaultIntegrationId, _defaultContextJson, &tr);
+    
+    tr.run(m_bc);
+}
+
+TEST_F(TestBCBlockchain, GetUniqs)
+{
+	TestResult tr;
+    Json::Value context;
+	Json::FastWriter fw;
+
+    m_bc->getBlockchainService()->GetUniqs(_defaultIntegrationId, _defaultContextJson, &tr);
+
+	tr.run(m_bc);
+}
