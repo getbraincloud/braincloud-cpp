a65d4af78830d5d78bee707cd6756e7b91c691f9
franco <82105876+francobithead@users.noreply.github.com>
2023-09-29 11:41:21 -0400

    Fix for failing Ultra test for InternalA

--- tests/src/TestBCAuth.cpp
+++ tests/src/TestBCAuth.cpp
@@ -283,10 +283,9 @@ TEST_F(TestBCAuth, AuthenticateAdvanced)
 
 TEST_F(TestBCAuth, AuthenticateUltra)
 {
-    if (TestFixtureBase::getServerUrl().find("api-internal.braincloudservers.com") == std::string::npos &&
-        TestFixtureBase::getServerUrl().find("internala.braincloudservers.com") == std::string::npos &&
-        TestFixtureBase::getServerUrl().find("api.internalg.braincloudservers.com") == std::string::npos/* &&
-        TestFixtureBase::getServerUrl().find("api.ultracloud.ultra.io") == std::string::npos*/)
+    if (TestFixtureBase::getServerUrl().find("api-internal.braincloudservers.com") == std::string::npos ||
+        TestFixtureBase::getServerUrl().find("api.internala.braincloudservers.com") == std::string::npos ||
+        TestFixtureBase::getServerUrl().find("api.internalg.braincloudservers.com") == std::string::npos)
     {
         printf("This env doesn't support Ultra authentication type\n");
         return;
