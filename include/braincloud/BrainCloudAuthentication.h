54fc65e23e5a566d9fe7adfd8b0b45af805f7e17
franco <82105876+francobithead@users.noreply.github.com>
2025-07-10 16:06:28 -0400

    Making compression responses and requests default on

--- include/braincloud/BrainCloudAuthentication.h
+++ include/braincloud/BrainCloudAuthentication.h
@@ -419,7 +419,7 @@ namespace BrainCloud
 		const std::string & getAnonymousId() const { return _anonymousId; }
 		const std::string & getProfileId() const { return _profileId; }
 
-		bool compressResponses = false;
+		bool compressResponses = true;
 
 		void setAnonymousId(const char * anonymousId) { _anonymousId = anonymousId; }
 		void setProfileId(const char * profileId) { _profileId = profileId; }
