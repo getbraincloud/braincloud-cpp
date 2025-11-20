8dd69a08059846e4db9d8a114dece0e6dc8d27fe
Nick H <nickh@bitheads.com>
2025-11-20 10:19:22 -0500

    Using better sleep function for delay between tests

--- tests/src/TestFixtureBase.cpp
+++ tests/src/TestFixtureBase.cpp
@@ -92,7 +92,7 @@ void TestFixtureBase::TearDown()
 	delete m_bcWrapper;
 
 	//Add delay at end of tests to ensure we do not get rate limited
-	this_thread::sleep_for(std::chrono::milliseconds(1000));
+	TestResult::sleep(1000);
 }
 
 bool TestFixtureBase::ShouldSkipAuthenticate()
